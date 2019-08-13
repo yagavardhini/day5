f0fc16313c74609dd1e77ad7e7dbee28bc0c262e
yagavardhini <48147119+yagavardhini@users.noreply.github.com>
2019-08-13 12:19:21 +0530

    Create frequency of each word.py

---
+++ frequency of each word.py
@@ -0,0 +1,16 @@
+def freq(str): 
+  
+    # break the string into list of words 
+    str_list = str.split() 
+  
+    # gives set of unique words 
+    unique_words = set(str_list) 
+      
+    for words in unique_words : 
+        print('Frequency of ', words , 'is :', str_list.count(words)) 
+  
+# driver code 
+if __name__ == "__main__": 
+      
+    str ='apple mango apple orange orange apple guava mango mango'
+    freq(str)   
