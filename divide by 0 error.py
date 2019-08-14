44c39cb28b17d085051c02b3ea59a33fab1bd75d
yagavardhini <48147119+yagavardhini@users.noreply.github.com>
2019-08-14 14:55:21 +0530

    Create divide by 0 error.py

---
+++ divide by 0 error.py
@@ -0,0 +1,10 @@
+def divide(x, y): 
+    try: 
+        
+        result = x // y 
+        print("Yeah ! Your answer is :", result) 
+    except ZeroDivisionError: 
+        print("Sorry ! You are dividing by zero ") 
+  
+
+divide(5, 0) 
